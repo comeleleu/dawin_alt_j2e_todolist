3c54b6f3d2662d79da4cf84a4de35d6158383f7e
Côme LELEU <comeleleu@users.noreply.github.com>
2017-02-01 23:05:21 +0100

    Ajout du projet

---
+++ todolist-presentation/src/main/java/fr/icdc/dei/todolist/controller/TodolistController.java
@@ -0,0 +1,51 @@
+package fr.icdc.dei.todolist.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import fr.icdc.dei.todolist.service.TodolistService;
+
+@Controller
+@RequestMapping("/")
+public class TodolistController {
+	
+	private static final String TASKS_HTTP_ATTR = "tasks";
+	
+	@Autowired
+	private TodolistService todolistService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView home() {
+		ModelAndView page = new ModelAndView("Home");
+		page.addObject(TASKS_HTTP_ATTR, todolistService.listTasks());
+		return page;
+	}
+	
+	@RequestMapping(value = "create", method=RequestMethod.GET)
+	public ModelAndView renderCreateTaskForm() {
+		ModelAndView page = new ModelAndView("TaskForm");
+		page.addObject("status", todolistService.listTaskStatus());
+		return page;
+	}
+	
+	@RequestMapping(value = "create", method=RequestMethod.POST)
+	public ModelAndView createTask(@RequestParam String taskName, @RequestParam("status") int statusId) {
+		ModelAndView page = new ModelAndView("Home");
+		todolistService.addTask(taskName, statusId);
+		page.addObject(TASKS_HTTP_ATTR, todolistService.listTasks());
+		return page;
+	}
+	
+	@RequestMapping(value = "/endtask/{id}", method=RequestMethod.GET)
+	public RedirectView finishTask(@PathVariable("id") Long id) {
+		todolistService.finishTask(id);
+		return new RedirectView("/todolist-presentation");
+	}
+
+}
---
+++ todolist-presentation/src/main/webapp/views/Home.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Todolist DAWIN alt</title>
+	</head>
+	
+	<body>
+	
+		<c:set var="wago" value="<%=new java.util.Date(System.currentTimeMillis() - (7 * 1000 * 60 * 60 * 24)) %>" />
+		
+	 	<c:forEach items="${tasks}" var="task">
+	 	
+			<div>
+				${task.name} - ${task.status.name}
+				<c:if test="${task.status.name != 'FINISHED' && task.beginDate le wago}">
+					<a href="<c:url value='/endtask/${task.id}' />">Terminer la tâche</a>
+				</c:if>
+			</div>
+		 
+		</c:forEach>
+	
+	</body>
+</html>
